c76e91df0eb1191c8cfb39f15241e77cdad5079d
marilianewton <mariliagomesnewton@gmail.com>
2024-07-01 22:35:32 -0300

    Add basic js functionality

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,6 @@ export default class APIcalls {
       `https://api.weatherapi.com/v1/forecast.json?key=0f969d81ee194864a4c161831242406&q=${cityName}&days=3&aqi=no&alerts=no, { mode: "cors" }`
     );
     const data = await res.json();
-    console.log(data);
-    console.log(data.forecast);
+    return data;
   }
 }
--- src/displayDom.js
+++ src/displayDom.js
@@ -2,11 +2,16 @@ import APIcalls from "./apiCalls";
 
 const citySearch = document.querySelector(".city-search");
 const searchBtn = document.querySelector(".search");
+const nextHoursContainer = document.querySelector(".next-hours-container");
+
+const dayCards = document.querySelectorAll(".day-card");
+const nextDaysContainer = document.querySelector(".next-days-container");
+const hourCards = document.querySelectorAll(".hour-card");
 
 const city = document.querySelector(".city-name");
 const country = document.querySelector(".country-name");
-const weekDay = document.querySelector(".week-day");
-const monthDay = document.querySelector(".month-day");
+const weekDayEl = document.querySelector(".week-day");
+const monthDayEl = document.querySelector(".month-day");
 const precipitation = document.querySelector(".precipitation");
 const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
@@ -16,25 +21,106 @@ const apiCalls = new APIcalls();
 
 export default class DisplayDOM {
   activeCity = "";
+  activeForecast = [];
+  activeData = {};
 
   init() {
-    searchBtn.addEventListener("click", this.searchNewCity);
+    searchBtn.addEventListener("click", () => this.searchNewCity());
+
     citySearch.addEventListener("keypress", (e) => {
       if (e.key === "Enter") {
         this.searchNewCity();
       }
     });
 
-    document
-      .querySelectorAll(".next-hours-container")
-      .forEach(this.pointerScroll);
+    dayCards.forEach((card) =>
+      card.addEventListener("click", () => {
+        const day = +card.dataset.day;
+        this.renderNextHours(day);
+        this.renderDate(day);
+        this.renderCurrentTemp(day);
+        this.renderWeatherData(day);
+      })
+    );
+
+    // Allow scroll on next hours forecast
+    this.pointerScroll(nextHoursContainer);
   }
 
-  searchNewCity() {
+  async searchNewCity() {
     if (!citySearch.value) return;
     this.activeCity = citySearch.value;
     citySearch.value = "";
-    apiCalls.searchCity(this.activeCity);
+    this.activeData = await apiCalls.searchCity(this.activeCity);
+    this.activeForecast = this.activeData.forecast.forecastday;
+
+    this.renderNext3Days();
+    this.renderCityCountry();
+    this.renderNextHours(0);
+    this.renderDate(0);
+    this.renderCurrentTemp(0);
+    this.renderWeatherData(0);
+  }
+
+  renderWeatherData(day) {
+    precipitation.textContent = `${this.activeForecast[day].day.totalprecip_mm}mm`;
+    humidity.textContent = `${this.activeForecast[day].day.avghumidity}%`;
+    wind.textContent = `${this.activeForecast[day].day.maxwind_kph} kph`;
+  }
+
+  renderCurrentTemp(day) {
+    degrees.innerHTML = `${Math.trunc(
+      this.activeForecast[day].day.avgtemp_c
+    )}&deg`;
+    degrees.previousElementSibling.src = `https:${this.activeForecast[day].day.condition.icon}`;
+  }
+
+  renderNextHours(day) {
+    const forecastByHour = this.activeForecast[day].hour;
+    hourCards.forEach((card, i) => {
+      card.querySelector(
+        ".weather-logo"
+      ).src = `https:${forecastByHour[i].condition.icon}`;
+      card.querySelector(".temp").innerHTML = `${Math.trunc(
+        forecastByHour[i].temp_c
+      )}&deg`;
+    });
+  }
+
+  renderCityCountry() {
+    city.textContent = this.activeData.location.name;
+    country.textContent = this.activeData.location.country;
+  }
+
+  renderDate(day) {
+    const date = new Date(this.activeForecast[day].date.split("-"));
+
+    const monthDay = date.getUTCDate();
+    const weekDay = date.toLocaleString("default", { weekday: "short" });
+    const month = date.toLocaleString("default", { month: "long" });
+
+    weekDayEl.textContent = `${weekDay},`;
+    monthDayEl.textContent = `${monthDay} ${month}`;
+  }
+
+  renderNext3Days() {
+    dayCards.forEach((day, i) => {
+      const date = new Date(this.activeForecast[i].date.split("-"));
+
+      const weekDay = date.toLocaleString("default", { weekday: "short" });
+
+      day.querySelector(".day").textContent = weekDay.toUpperCase();
+      day.querySelector(
+        ".weather-logo"
+      ).src = `https:${this.activeForecast[i].day.condition.icon}`;
+
+      day.querySelector(
+        ".max"
+      ).innerHTML = `${this.activeForecast[i].day.maxtemp_c}&deg`;
+      day.querySelector(
+        ".min"
+      ).innerHTML = `${this.activeForecast[i].day.mintemp_c}&deg`;
+    });
   }
 
   pointerScroll = (elem) => {
